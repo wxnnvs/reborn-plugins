74f7130479486cbf2e793d95e349cfeff051b4a1
wxnnvs <215158@proton.me>
2023-10-26 17:20:03 +0200

    GODMODEEE

---
+++ godmode.js
@@ -0,0 +1,12 @@
+//basicly no damage
+PluginAPI.require("player");
+
+var isToggled = false; //stores whether the plugin is on or off
+
+PluginAPI.addEventListener("key", (event) => {
+    if (event.key === 34) { // Key code for 'g'
+        isToggled = !isToggled;
+        PluginAPI.player.capabilities.disableDamage = isToggled;
+        PluginAPI.updateComponent("player");
+    }
+});
