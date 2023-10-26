7d5e583924035e706c7dc2c22251dabae3247dbc
wxnnvs <215158@proton.me>
2023-10-26 17:35:33 +0200

    a fix?

--- godmode.js
+++ godmode.js
@@ -4,9 +4,10 @@ PluginAPI.require("player");
 var isToggled = false; //stores whether the plugin is on or off
 
 window.addEventListener("keydown", (event) => {
-    if (event.key === 34) { // Key code for 'g'
+    if (event.key.toLowerCase() === "h") {
         isToggled = !isToggled;
         PluginAPI.player.capabilities.disableDamage = isToggled;
-        PluginAPI.updateComponent("player");
+        PluginAPI.player.reload();
+        PluginAPI.displayToChat({msg: "Godmode toggled " + isToggled})
     }
 });
