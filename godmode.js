ef7f2bc23ad41a41e7860809bb9427654851a58a
wxnnvs <215158@proton.me>
2023-10-26 17:27:50 +0200

    test

--- godmode.js
+++ godmode.js
@@ -3,7 +3,7 @@ PluginAPI.require("player");
 
 var isToggled = false; //stores whether the plugin is on or off
 
-PluginAPI.addEventListener("key", (event) => {
+window.addEventListener("keypress", (event) => {
     if (event.key === 34) { // Key code for 'g'
         isToggled = !isToggled;
         PluginAPI.player.capabilities.disableDamage = isToggled;
