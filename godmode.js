6c35623b8268ef7afb05fd4f2579abc5249440f3
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2024-02-23 12:35:41 +0100

    idk

--- godmode.js
+++ godmode.js
@@ -3,7 +3,7 @@ NOTE: This will only work in singleplayer
 */
 ModAPI.require("player");
 
-var isToggled = false; //stores whether the plugin is on or off
+var isToggled = false; // stores whether the plugin is on or off
 
 window.addEventListener("keydown", (event) => {
     if (event.key.toLowerCase() === "h") {
