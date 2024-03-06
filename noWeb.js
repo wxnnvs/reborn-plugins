dad49180a6e40261cf138d8c332a1b61106eac37
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2024-03-06 19:13:44 +0100

    tetskosùk

--- noWeb.js
+++ noWeb.js
@@ -6,11 +6,20 @@ ModAPI.require("player");
 
 var isToggled = false; // stores whether the plugin is on or off
 
+function updatePlayerWeb() {
+    ModAPI.player.setInWeb(isToggled);
+    ModAPI.player.reload();
+}
+
+// add the update event listener
+window.addEventListener("update", updatePlayerWeb);
+
+// add the keydown event listener
 window.addEventListener("keydown", (event) => {
     if (event.key.toLowerCase() === "h") {
+        window.removeEventListener("update", updatePlayerWeb);
         isToggled = !isToggled;
-        ModAPI.player.setInWeb(isToggled);
-        ModAPI.player.reload();
+        window.addEventListener("update", updatePlayerWeb);
         ModAPI.displayToChat({msg: "NoWeb toggled " + isToggled})
     }
 });
--- teleport.js
+++ teleport.js
@@ -1,13 +1,11 @@
 /*
-    Broken on EaglerForge's side
+    fixed?
 */
 ModAPI.require("player");
 
 // handle message and teleport player
-ModAPI.addEventListener("sendchatmessage", (e) => {
-    let msg = e.message;
-    if (msg === ".teleport") {
-        e.preventDefault = true;
+ModAPI.addEventListener("key", (e) => {
+    if (e.key == 49) {
         let X = prompt("Teleporter v1\nInput an X-coordinate");
         let Y = prompt("Teleporter v1\nInput a Y-coordinate");
         let Z = prompt("Teleporter v1\nInput a Z-coordinate");
@@ -15,9 +13,6 @@ ModAPI.addEventListener("sendchatmessage", (e) => {
             x: X,
             y: Y,
             z: Z
-        });
-        ModAPI.sendToChat({
-            msg: "teleported to desired location"
         });
     }
 });
